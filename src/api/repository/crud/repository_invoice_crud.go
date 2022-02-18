6607e711361eb5b6809bda031c1ab01424ffd01c
amanc1361 <rahymrm@gmail.com>
2022-02-18 23:00:05 +0330

    add method to invoice

--- src/api/repository/crud/repository_invoice_crud.go
+++ src/api/repository/crud/repository_invoice_crud.go
@@ -2,6 +2,7 @@ package crud
 
 import (
 	"back-account/src/api/models"
+	"back-account/src/api/utils/channels"
 
 	"gorm.io/gorm"
 )
@@ -15,7 +16,81 @@ func NewInvocieRepository(db *gorm.DB) *invoiceRepository {
 }
 
 func (r *invoiceRepository) Save(invoice models.Invocie) (models.Invocie,error) {
-//todo 
-return models.Invocie{},nil
+var err error
+done:=make(chan bool)
+invoicenumber,err:=r.GetLastInvoiceNumber(invoice.CompanyId,invoice.YearId,2)
+if err!=nil {
+	return models.Invocie{},err
+}
 
+invoice.InvoiceNumber=invoicenumber+1
+go func(ch chan<-bool) {
+    err:=r.db.Create(&invoice).Error
+	if err!=nil {
+		ch<-false 
+		return 
+	}
+	ch<-true
+}(done)
+
+if channels.Ok(done) {
+	return invoice,err
+}
+return models.Invocie{},err
+
+}
+
+func (r *invoiceRepository ) GetLastInvoiceNumber(companyid int,yearid int,invoicetype int)(int,error) {
+	var err error 
+	done:=make(chan bool)
+	var invoicenumber int
+
+	go func(ch chan<-bool) {
+		err=r.db.Select("invoiceNumber").Where("companyId=? and year_id=? and invoiceType=?",companyid,yearid,invoicetype).Take(&invoicenumber).Error
+		if err!=nil {
+			ch<-false
+			return
+		}
+		ch<-true
+	}(done)
+	if channels.Ok(done) {
+		return invoicenumber,err
+	}
+	return 0,err
+}
+
+func (r *invoiceRepository) GetAll(companyid int,yearid int,invoicetype int)([]models.Invocie,error) {
+	var err error
+	 invoices :=[]models.Invocie{}
+	 done:=make(chan bool)
+	go func(ch chan<-bool) {
+		err=r.db.Where("companyId=? and yearId=? and invoiceType=?",companyid,yearid,invoicetype).Find(&invoices).Error
+		if err!=nil {
+			ch<-false
+			return
+		}
+		ch<-true
+	}(done)
+	if channels.Ok(done) {
+		return invoices,err
+	}
+	return []models.Invocie{},err
+}
+
+func (r *invoiceRepository) GetInvocie(invoiceid int)(models.Invocie,error) {
+	var err error
+	var invoice models.Invocie
+	done:=make(chan bool)
+	go func(ch chan<-bool) {
+		err=r.db.Where("id=?",invoiceid).Take(&invoice).Error
+		if err!=nil {
+			ch<-false
+			return
+		}
+		ch<-true
+	}(done)
+	if channels.Ok(done) {
+		return invoice,err
+	}
+	return models.Invocie{},err
 }
--- src/api/repository/repository_invoice.go
+++ src/api/repository/repository_invoice.go
@@ -3,11 +3,12 @@ package repository
 import "back-account/src/api/models"
 
 type Inovice interface {
-	Save(invoice models.Invocie)(models.Invocie,error)
+	Save( invoice models.Invocie)(models.Invocie,error)
 	Delete(invoiceid int)(int,error)
 	Update(invoice models.Invocie,invoiceid int)(models.Invocie,error)
 	GetInvocie(invoiceid int)(models.Invocie,error)
 	GetAll(companyid int,yeaid int)([]models.Invocie,error)
+	GetLastInvoiceNumber(companyid int,yearid int,invoicetype int)(int,error)
 	
 
 }
