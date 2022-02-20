3e0d0f305ebab8dd27ffdc16e99a3622753374c4
amanc1361 <rahymrm@gmail.com>
2022-02-20 13:30:34 +0330

    modify Invoice model

--- src/api/models/invoice.go
+++ src/api/models/invoice.go
@@ -12,7 +12,9 @@ type Invoice struct {
 	InvoiceTypeId      int `json:"invoiceTypeId"`
 	Amount           int `json:"amount"`
 	Description      string `json:"description"`
-	
+	Tax int `json:"tax"`
+	ValueAdd int `json:"value_add"`
+	ValueExtended int `json:"value_extended"`
 	CompanyId int `json:"companyId"`
 	YearId int  `json:"yearId"`
 	InvoiceRows []InvoiceRow
