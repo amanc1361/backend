adad6d28ee394f855f49de72dafaf863b99e3050
amanc1361 <rahymrm@gmail.com>
2022-02-20 22:22:44 +0330

    change filed model invoicerows

--- src/api/models/invoicerows.go
+++ src/api/models/invoicerows.go
@@ -6,6 +6,7 @@ type InvoiceRow struct {
 	gorm.Model
 	InvoiceID  uint `json:"invoice_id"`
 	ObjectId int `json:"object_id"`
+	Code int `json:"code"`
 	Name string `json:"name"`
 	Unitname string  `json:"unit_name"`
 	Count int   `json:"count"`
