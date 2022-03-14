e4426c6bd254ee5a01bcd7f4950bfefa9fbe88b5
amanc1361 <rahymrm@gmail.com>
2022-03-14 17:37:35 +0330

    add another method for account

--- src/api/repository/crud/repository_account_crud.go
+++ src/api/repository/crud/repository_account_crud.go
@@ -33,3 +33,62 @@ func (r *repositoryAccountCrud) Save(account models.Account)(models.Account,erro
 	}
 	return models.Account{},err
 }
+
+func (r *repositoryAccountCrud) Update(account models.Account) (models.Account ,error) {
+	var err error
+    done:=make(chan bool)
+
+	go func(ch chan<-bool) {
+		 err=r.db.Updates(&account).Error
+		 if err!=nil {
+			 ch<-false
+			 return
+		 }
+		 ch<-true
+	}(done)
+	if channels.Ok(done) {
+		return account,nil
+
+	}
+	return models.Account{},err
+	  
+}
+
+func (r *repositoryAccountCrud) Delete(accountid int)error {
+	var err error
+    done:=make(chan bool)
+
+	go func(ch chan<-bool) {
+		 err=r.db.Delete(models.Account{},accountid).Error
+		 if err!=nil {
+			 ch<-false
+			 return
+		 }
+		 ch<-true
+	}(done)
+	if channels.Ok(done) {
+		return nil
+
+	}
+	return err
+}
+func (r *repositoryAccountCrud) Get(accountid int) (models.Account,error) {
+	var err error
+    done:=make(chan bool)
+	account:=models.Account{}
+
+	go func(ch chan<-bool) {
+		 err=r.db.Model(models.Account{}).Where("id=?",accountid).First(&account).Error
+		 if err!=nil {
+			 ch<-false
+			 return
+		 }
+		 ch<-true
+	}(done)
+	if channels.Ok(done) {
+		return account,nil
+
+	}
+	return models.Account{},err
+
+}
