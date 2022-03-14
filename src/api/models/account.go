d4aef5e8901164130cd786984c582de26d15cf2c
amanc1361 <rahymrm@gmail.com>
2022-03-14 16:31:40 +0330

    change account model

--- src/api/models/account.go
+++ src/api/models/account.go
@@ -1,14 +1,16 @@
-packge models
-type Account struct {
-   gorm.models
-   AccountNumber string `json:"account_number"`
-   AccountType int `json:"account_type"`
-   DetailedName string `json:"detailed_name"`
-   DetailedId int `json:"detailed_id"`
-   Owner string `json:"owner"`
-   CreateSolarDate string `json:"create_solar_date"`
-   IsActive bool `json:"is_active"`
-   ShebaNumber string `json:"sheba_number"`
-   CardNumber string `json:"card_number"`
+package models
 
+import "gorm.io/gorm"
+
+type Account struct {
+	gorm.Model
+	AccountNumber   string `json:"account_number"`
+	AccountType     int    `json:"account_type"`
+	DetailedName    string `json:"detailed_name"`
+	DetailedId      int    `json:"detailed_id"`
+	Owner           string `json:"owner"`
+	CreateSolarDate string `json:"create_solar_date"`
+	IsActive        bool   `json:"is_active"`
+	ShebaNumber     string `json:"sheba_number"`
+	CardNumber      string `json:"card_number"`
 }
