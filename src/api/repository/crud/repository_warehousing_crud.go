63ba0d7218a2b80bdea0f553a93e4fdc7ef3ad47
amanc1361 <rahymrm@gmail.com>
2022-04-30 11:43:36 +0430

    add find method for warehousing

--- src/api/repository/crud/repository_warehousing_crud.go
+++ src/api/repository/crud/repository_warehousing_crud.go
@@ -25,3 +25,24 @@ func (r *repositoryWarehousingCRUD) Save(models.WareHousing warehousing) (models
 	 return models.WareHousing{},err
 
 }
+
+func (r *repositoryWarehousingCRUD) FindAll(comapnyid int, yearid int)([]models.WareHousing,error) {
+  var err error 
+
+  done:=make(chan bool)
+  warehousings:=[]models.warehousing{}
+  go func (ch chan<-bool) {
+	  err=r.db.Where("company_id=? and year_id=?",comapnyid,yearid).Find(&warehousings).Error
+	  if err!=nil {
+		  ch<-false
+		  return
+	  }
+	  ch<-true 
+
+  }(done)
+   if channels.Ok(done) {
+	   return warehousings,nil
+   }
+   return []models.WareHousing{},err
+	
+ }
