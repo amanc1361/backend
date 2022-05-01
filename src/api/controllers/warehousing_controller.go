5c0b783ffa8115649c3e2c7132d1a47f63bf5ac7
amanc1361 <rahymrm@gmail.com>
2022-05-01 10:22:11 +0430

    add method save to controller warehouse

--- src/api/controllers/warehousing_controller.go
+++ src/api/controllers/warehousing_controller.go
@@ -1,7 +1,75 @@
 package controllers
 
-import "net/http"
+import (
+	"back-account/src/api/database"
+	"back-account/src/api/models"
+	"back-account/src/api/repository"
+	"back-account/src/api/repository/crud"
+	"back-account/src/api/responses"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+)
 
 func GetWareHousings(w http.ResponseWriter, r *http.Request) {
+	var v = r.URL.Query()
+	yearid, err := strconv.ParseUint(v.Get("yearid"), 10, 32)
+	companyid, err := strconv.ParseUint(v.Get("companyid"), 10, 32)
+		if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	db, err := database.Connect()
+	if err != nil {
+		responses.ERROR(w, http.StatusInternalServerError, err)
+		return
+	}
+	sqlDB, err := db.DB()
+	defer sqlDB.Close()
+	repo:=crud.NewRepositoryWareHousingCRUD(db)
+	func(wareHousing repository.WareHousingRepository) {
 
+		warehusings, err := wareHousing.FindAll(int(companyid),int(yearid))
+		if err != nil {
+			responses.ERROR(w, http.StatusUnprocessableEntity, err)
+			return
+		}
+
+		responses.JSON(w, http.StatusOK, warehusings)
+	}(repo)
+}
+func SaveWareHouse(w http.ResponseWriter,r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	warehouing := models.WareHousing{}
+	err = json.Unmarshal(body, &warehouing)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	db, err := database.Connect()
+	sqlDB, err := db.DB()
+	defer sqlDB.Close()
+	if err != nil {
+		responses.ERROR(w, http.StatusInternalServerError, err)
+		return
+	}	
+	repo:=crud.NewRepositoryWareHousingCRUD(db)
+	func(wareHousing repository.WareHousingRepository) {
+		warehouing, err = wareHousing.Save(warehouing)
+		if err != nil {
+			responses.ERROR(w, http.StatusUnprocessableEntity, err)
+			return
+		}
+		responses.JSON(w, http.StatusOK, wareHousing)
+	}(repo)
+}
+
+func UpdateWareHouse(w http.ResponseWriter,r *http.Request) {
+	
 }
